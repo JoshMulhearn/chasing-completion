bbd06966733b0bc5dd141861a3cd82a9b7308a3c
Josh Mulhearn <Tonner906@gmail.com>
2025-04-23 15:23:57 +0100

    footer to be changed later

---
+++ footer.php
@@ -0,0 +1,25 @@
+<footer>
+    <div class="footer-container">
+        <div class="footer-column">
+            <p>Chasing Completion</p>
+            <p>completionist@cc.com</p>
+            <p>+44 7245273732</p>
+        </div>
+        <div class="footer-column">
+            <ul>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Privacy Policy</a></li>
+                <li><a href="#">Terms and Conditions</a></li>
+            </ul>
+        </div>
+        <div class="footer-column">
+            <div class="steam-branding">
+                <img src="Images/steam-icon.png" alt="Steam Logo" class="steam-logo">
+                <div class="steam-text">
+                    <span>Powered</span>
+                    <span>by Steam</span>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
