79c7945ddcb6e62030b229ab09957a99b8ef60cb
Josh Mulhearn <Tonner906@gmail.com>
2025-04-23 11:48:14 +0100

    uploaded initial commit

---
+++ footer.php
@@ -0,0 +1,21 @@
+<footer>
+    <div class="footer-section-container">
+        <div class="section">
+            <h4>Chasing Completion</h4>
+            <p>completionist@cc.com</p>
+            <p>+44 7425321243</p>
+            <br>
+        </div>
+        <div class="vl"></div>
+        <div class="section">
+            <h4>Links</h4>
+            <a href="#">About</a>
+            <a href="#">Privacy Policy</a>
+            <a href="#">Terms and Conditions</a>
+        </div>
+        <div class="vl"></div>
+        <div class="section">
+            <h4 class="pbs">Powered By Steam</h4>
+        </div>
+    </div>
+</footer>
---
+++ navbar.php
@@ -0,0 +1,20 @@
+
+<nav class="navbar">
+    
+    <img class="bar-menu" src="images/hamburger-icon.svg" alt="bar-menu">
+    
+    <div class="logo-headers">
+        <h1>Chasing</h1>
+        <h1>Completion</h1>
+    </div>
+    <ul>
+        <a href="#">Link</a>
+        <a href="#">Link</a>
+        <a href="#">Link</a>
+        <a href="#">Link</a>
+    </ul>
+
+    <button class="profile-btn">
+        <img src="" alt="profile">
+    </button>
+</nav>
