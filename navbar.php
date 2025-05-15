7eff29add57f25a21f5d0f2acf21818132deb120
Josh Mulhearn <tonner906@gmail.com>
2025-05-16 00:55:55 +0100

    navbar now includes a logout link when logged in and also changes the home link to dashboard.php when logged in

--- navbar.php
+++ navbar.php
@@ -1,5 +1,20 @@
+<?php     
+    if (session_status() == PHP_SESSION_NONE) 
+    //since navbar is included in every page and a session may have already been started, 
+    // this code only starts a session when one hasnt already been created 
+    {
+        session_start();
+    }
 
+    //default home link is index.php
+    $home_link = "index.php";
 
+    if(isset($_SESSION['logged_in']))
+    {
+        //home link changes to dashboard.php when user is logged in
+        $home_link = "dashboard.php";
+    }
+?>
 
 <nav class="navbar">
     
@@ -19,15 +34,27 @@
         <button class="profile-btn">
          
         </button>
-        <img class="profile-btn-img" src="images/profile-placeholder-1.svg" alt="profile">
+        <img class="profile-btn-img" src="images/profile-placeholder-1.svg" alt="profile"> <!--change to logged in avatar and also add functionality-->
     </div>
 </nav>
 
 <div class="off-screen-menu">
     <ul>
-        <li><a href="index.php">Home</a></li>
+        <li><a href="<?php echo $home_link;?>">Home</a></li> <!--Home link is either index.php or dashboard.php depending on wether a user is logged in-->
+        <?php //maybe a tidier way to do this but reusing isset because it works 
+            if(isset($_SESSION['logged_in']))
+            {
+                echo "<li><a href='#'</a></li>";
+            }
+        ?>
         <li><a href="#">About</a></li>
         <li><a href="#">Privacy Policy</a></li>
         <li><a href="#">Terms and Conditions</a></li>
+        <?php 
+            if(isset($_SESSION['logged_in']))
+            {
+                echo "<li><a href='logout.php'</a>Logout</li>";
+            }
+        ?>
     </ul>
 </div>
